8d609063641752a327e10c4abd039409d8a2f3a8
Kyle Chen <kyle.h.chen@gmail.com>
2017-10-05 13:45:06 -0700

    Fix level-8

--- board.rb
+++ board.rb
@@ -1,6 +1,7 @@
 require_relative "tile"
 
 class Board
+
   def self.empty_grid
     Array.new(9) do
       Array.new(9) { Tile.new(0) }
@@ -42,7 +43,7 @@ class Board
       puts "#{i} #{row.join(" ")}"
     end
   end
-
+  
   def rows
     grid
   end
@@ -55,7 +56,7 @@ class Board
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
-    end
+
   end
 
   def solved_set?(tiles)
